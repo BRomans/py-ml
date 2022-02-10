51018c6888bbbd31bf9c2b6e4bcb7a7c3e399971
BRomans <michele.romani.gzl0@gmail.com>
2022-02-10 12:00:00 +0100

    :sparkles: implemented example of KNN using SBS on the wine dataset.

---
+++ examples/chapter_4/knn_sbs.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from examples.chapter_4.sequential_backward_selection import SBS
+
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+df_wine.columns = ['Class label',
+                   'Alcohol',
+                   'Malic Acid',
+                   'Ash',
+                   'Alcalinity of ash',
+                   'Magnesium',
+                   'Total phenols',
+                   'Flavanoids',
+                   'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity',
+                   'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline']
+print('Class labels', np.unique(df_wine['Class label']))
+print(df_wine.head())
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Standardization with StandardScaler
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=2)
+sbs = SBS(knn, k_features=1)
+sbs.fit(X_train_std, y_train)
+
+# Plotting
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.1])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.show()
+
+# Return the features that achieved the best accuracy on the validation dataset
+k5 = list(sbs.subsets_[8])
+print(df_wine.columns[1:][k5])
+
+# Evaluate the performance of KNN classifier on the original test set
+knn.fit(X_train_std, y_train)
+print('Training accuracy:', knn.score(X_train_std, y_train))
+print('Test accuracy:', knn.score(X_test_std, y_test))
+
+# Evaluate again using the 5 selected features
+print('Selecting 5 best features...')
+knn.fit(X_train_std[:, k5], y_train)
+print('Training accuracy:', knn.score(X_train_std[:, k5], y_train))
+print('Test accuracy:', knn.score(X_test_std[:, k5], y_test))
